dc0ea607b16733c8ef32899c97c603de2018f725
Devon Morrison <devmorri421@gmail.com>
2020-01-10 16:41:42 -0500

    Added failed test

---
+++ src/main/java/MobsterGameApplication.java
@@ -0,0 +1,15 @@
+import mob.MobParty;
+import model.PlayerCharacter;
+
+import java.util.ArrayList;
+
+public class MobsterGameApplication {
+
+    public static void main(String [] args) {
+
+    }
+
+
+
+
+}
---
+++ src/main/java/mob/MobParty.java
@@ -0,0 +1,15 @@
+package mob;
+
+import model.PlayerCharacter;
+
+import java.util.ArrayList;
+
+public class MobParty {
+
+    ArrayList<PlayerCharacter> party = new ArrayList<PlayerCharacter>();
+
+    public MobParty(ArrayList<PlayerCharacter> party) {
+        this.party = party;
+    }
+
+}
